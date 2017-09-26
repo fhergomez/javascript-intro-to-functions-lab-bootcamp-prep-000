140e6e41fa61e1bed94ce316900d05a4965897bf
Fernando Gomez <fhergomez37@gmail.com>
2017-09-26 04:58:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,13 +15,9 @@ function logWhisper (string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase()) {
-    return "I can't hear you!";
-  }
-}
-
-function sayHiToGrandma(string) {
   if (string.toUpperCase()) {
     return "YES INDEED!";
+  } esle {
+    return "I can't hear you!";
   }
 }
