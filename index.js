41233694ba0c6a7b3503422bd23656d5de9cbf51
Fernando Gomez <fhergomez37@gmail.com>
2017-09-26 05:03:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ function sayHiToGrandma(string) {
   if (string.toUpperCase()) {
     return "YES INDEED!";
   } else if (string.toLowerCase()){
-    return "I can't hear you!";
+    return "I can\'t hear you!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too";
   }
 }
